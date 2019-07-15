fdcc2b2644468cfef52efa87a375e4f3db055c8a
Altamirano <Altamirano@DESKTOP-94VPAJ9>
2019-07-14 23:56:28 -0600

    Entrada y salida

--- Estacionamiento/Estacionamiento/Estacionamiento/Registro.xaml.cs
+++ Estacionamiento/Estacionamiento/Estacionamiento/Registro.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,39 @@ namespace Estacionamiento
     /// </summary>
     public partial class Registro : Page
     {
+        SqlConnection sqlconnection;
+
         public Registro()
         {
             InitializeComponent();
+        }
+
+        private void BtnListar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connectionString = @"Server=(local)\sqlexpress; database = Estacionamiento; Integrated Security=true;";
+                sqlconnection = new SqlConnection(connectionString);
+                SqlCommand query = new SqlCommand(@"SELECT Vehiculo.placa, 
+	                                                   TipoVehiculo.tipo, 
+                                                       PagoVehiculo.fechaHoraEntrada,
+	                                                   PagoVehiculo.fechaHoraSalida, 
+	                                                   PagoVehiculo.total
+                                                       FROM Estacionamiento.Vehiculo INNER JOIN Estacionamiento.PagoVehiculo
+                                                       ON Estacionamiento.PagoVehiculo.vehiculo = Estacionamiento.Vehiculo.id INNER JOIN Estacionamiento.TipoVehiculo
+                                                       ON Estacionamiento.TipoVehiculo.id = Estacionamiento.Vehiculo.tipoVehiculo", sqlconnection);
+                sqlconnection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query);
+                DataTable registros = new DataTable();
+                sqlDataAdapter.Fill(registros);
+                dgRegistros.ItemsSource = registros.DefaultView;
+                query.Dispose();
+                sqlconnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
